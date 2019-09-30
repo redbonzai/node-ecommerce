40e3bbeb1f0be9849411cfa5f5303a728f425dc4
Christian Dominguez <christian@redbonzai.com>
2019-09-29 21:18:42 -0500

    Products:  Created crud methods for Products

--- app/controllers/ProductController/index.js
+++ app/controllers/ProductController/index.js
@@ -13,7 +13,7 @@ export const getAllProducts = (req, res) => {
             })
         }
 
-        return res.json({ result })
+        return res.json({ result: result })
     })
 }
 
@@ -63,4 +63,96 @@ export const createProduct = (req, res) => {
         })
     })
 
+}
+
+export const getProductById = (req, res) => {
+    req.product.photo = undefined
+    return res.json(req.product)
+}
+
+export const productById = (req, res, next, id) => {
+    Product.findById(id).exec((err, product) => {
+        if (err || !product) {
+            return res.status(400).json({
+                error: "Product not found"
+            })
+        }
+
+        req.product = product
+        next()
+    })
+}
+
+export const updateProduct = (req, res) => {
+    let form = new formidable.IncomingForm()
+    form.keepExtensions = true
+    form.parse(req, (err, fields, files) => {
+        if (err) {
+            return res.status(400).json({
+                error: 'Image could not be uploaded'
+            })
+        }
+
+        // Check that all fields are filled
+        const { name, description, price, category, quantity, shipping } = fields
+
+        if (!name || !description || !price || !category || !quantity || !shipping ) {
+            return res.status(400).json({
+                error: 'All fields are required'
+            })
+        }
+
+
+        
+        let product = req.product
+        product = _.extend(product, fields)
+
+        console.log('updated Product: ', product)
+
+        if (files.photo) {
+
+            // check that image files are less than 1MB
+            if (files.photo.size > 1000000) {
+                return res.status(400).json({
+                    error: "Image size should be less than 1MB"
+                })
+            }
+
+            product.photo.data = fs.readFileSync(files.photo.path)
+            product.photo.contentType = files.photo.type
+        }
+
+        product.save((err, result) => {
+            console.log('update result: ', result)
+            console.error('update error: ', err)
+            if (err || ! result) {
+                return res.status(400).json({
+                    error: err.message,
+                    message: 'The product was not saved'
+                })
+            }
+
+            result.photo = undefined
+
+            res.json(result)
+        })
+    })
+
+}
+
+export const deleteProduct = (req, res) => {
+    req.product.photo = undefined
+    let product = req.product
+    product.remove((err, deletedProduct) => {
+        if (err) {
+            return res.status(400).json({
+                error: errorHandler(err)
+            })
+        }
+
+        res.json({
+            deletedProduct: product,
+            message: 'Product has been deleted'
+        })
+    })
 }
