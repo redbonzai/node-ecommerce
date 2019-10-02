9bcbb588b9dbb43a3320f30c8a19246f64651b40
Christian Dominguez <christian@redbonzai.com>
2019-10-02 16:01:04 -0500

    CategoryCrud: Added routes for getting category by ID, and for creating a category.

--- app/routes/category.js
+++ app/routes/category.js
@@ -6,7 +6,8 @@ import { requireSignin, isAuth, isAdmin } from '../controllers/AuthController'
 import { userById } from '../controllers/UserController'
 
 router.get('/category', requireSignin, getCategories)
-router.post('/category/create/:userId', requireSignin, isAdmin, createCategory)
+router.get('/category/:categoryId', requireSignin, getCategoryById)
+router.post('/category/create/:userId', requireSignin, isAuth, isAdmin, createCategory)
 
 router.param('userId', userById)
 
